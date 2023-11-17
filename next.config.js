848e5f7442d055b4994994478118f1d7148fc66c
Luiz Guilherme Reis <luiz.guilherme.dev@outlook.com>
2023-11-17 14:37:35 -0300

    Fisrt commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "boutiqueapexlegends.fr",
+      "images6.alphacoders.com",
+      "images.contentstack.io",
+      "a-static.besthdwallpaper.com",
+      "cdnportal.mobalytics.gg",
+      "images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com",
+      "www.gamespot.com",
+      "media.valorant-api.com",
+      "valorant.dyn.riotcdn.net",
+    ],
+  },
+};
+
+module.exports = nextConfig;
